706491995b66e9ddd854fb814904bb602abfc4cd
Wuggie <kawog17@student.sdu.dk>
2020-10-30 23:52:45 +0100

    test af fast lagt patrol

--- Fysik-el-VR/Assets/Scripts/TestNavMeshScript.cs
+++ Fysik-el-VR/Assets/Scripts/TestNavMeshScript.cs
@@ -5,7 +5,9 @@ using UnityEngine.AI;
 
 public class TestNavMeshScript : MonoBehaviour
 {
-
+    public Transform[] points;
+    private int destPoints = 0;
+    public int searchDistance;
     public Transform target;
     public NavMeshAgent agent;
 
@@ -13,11 +15,45 @@ public class TestNavMeshScript : MonoBehaviour
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+
+        GotoNextPoint();
+
+        //prints the distance between the robot and the player
+        Debug.Log("Distance to player: " + Vector3.Distance(target.position, transform.position));
     }
 
     // Update is called once per frame
     void Update()
     {
-        agent.SetDestination(target.position);
+        //Sets the distance variable, to be the distance between the player and the robot.
+        var distance = Vector3.Distance(target.position, transform.position);
+
+        //checks if the distance between the robot and the limit of it's search is true.
+        if(distance <= searchDistance)
+        {
+            //makes the robot move towards it's target.
+            agent.SetDestination(target.position);
+        }
+
+        else if (!agent.pathPending && agent.remainingDistance < 0.5f)
+        {
+            GotoNextPoint();
+        }
+
+    }
+
+    void GotoNextPoint()
+    {
+        //returns if no points have been set up
+        if (points.Length == 0)
+            return;
+
+        //makes the robot go to the current selected destination.
+        agent.destination = points[destPoints].position;
+
+        //picks the next point in the array at the destination, cycling to the start if needed.
+        destPoints = (destPoints + 1) % points.Length;
+
+
     }
 }
