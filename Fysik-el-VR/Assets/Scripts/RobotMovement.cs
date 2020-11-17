0c4db3a4593f44a22ef3999a0e16c28bf9348384
Wuggie <wuggie88@yahoo.co.uk>
2020-11-17 13:52:40 +0100

    player targeting fix.

--- Fysik-el-VR/Assets/Scripts/RobotMovement.cs
+++ Fysik-el-VR/Assets/Scripts/RobotMovement.cs
@@ -32,7 +32,7 @@ public class RobotMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.Find("Player").transform;
+        player = GameObject.FindWithTag("Player").transform;
         agent = GetComponent<NavMeshAgent>();
     }
 
