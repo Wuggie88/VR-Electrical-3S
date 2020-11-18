d2ebf90afdd511b7480803c4965ca57245556119
Jejak-14 <jejak14@student.sdu.dk>
2020-11-18 11:17:29 +0100

    Oprettet et healthsystem
    skulle kunne virke på både player og Robot

---
+++ Fysik-el-VR/Assets/Scripts/Healthsystem.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Healthsystem : MonoBehaviour
+{
+    
+    public int health;
+    public int damage;
+
+    public Collider FjendtligSkud;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 100;
+    }
+
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other == FjendtligSkud)
+        {
+            TakeDamage();
+        }
+    }
+
+    public void TakeDamage()
+    {
+
+        health -= damage;
+
+
+        if (health <= 0)     Death(); 
+
+    }
+
+    public void Death()
+    {
+        if (this.tag == "Player")
+        {
+            //hvis en dødsscene
+        }
+        else
+        {
+            Destroy(this);
+        }
+        
+    }
+}
