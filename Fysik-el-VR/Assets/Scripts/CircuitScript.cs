8ef8cb61a41da92dba188a7456ff16f8e0d1bcc0
Wuggie <wuggie88@yahoo.co.uk>
2020-11-24 13:17:20 +0100

    No clue what the fuck this is

--- Fysik-el-VR/Assets/Scripts/CircuitScript.cs
+++ Fysik-el-VR/Assets/Scripts/CircuitScript.cs
@@ -8,15 +8,25 @@ public class CircuitScript : MonoBehaviour
 
     //the current points of the circuit
     public float doorpoints = 4;
+    public GameObject battery;
+    public GameObject R1;
+    public GameObject R2;
+    public GameObject R3;
+    public float batteryValue;
+    public float R1Value;
+    public float R2Value;
+    public float R3Value;
 
     //the target needed for the circuit to be correctly solved
     public float target = 5;
 
 
+    public float[] r;
 
     // Start is called before the first frame update
     void Start()
     {
+        
         
     }
 
@@ -26,6 +36,27 @@ public class CircuitScript : MonoBehaviour
         
     }
 
+
+    public void setupComponent()
+    {
+        StartCoroutine(calculateThisBitch());
+
+
+    }
+
+    IEnumerator calculateThisBitch()
+    {
+        batteryValue = battery.GetComponent<Snapping>().values;
+        R1Value = R1.GetComponent<Snapping>().values;
+        R2Value = R2.GetComponent<Snapping>().values;
+        R3Value = R3.GetComponent<Snapping>().values;
+
+        yield return new WaitForEndOfFrame();
+
+        doorpoints = batteryValue * (R1Value + R2Value + R3Value);
+
+
+    }
     /*
      * Use this as the Circuit script
      * The door button will set the requirement as "target" so set that in your prefab for what the solution should be.
