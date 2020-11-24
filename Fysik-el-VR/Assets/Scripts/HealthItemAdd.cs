d83974b7f1c6956b3da96be9d0575c8a63793cd5
Jejak-14 <jejak14@student.sdu.dk>
2020-11-24 13:37:59 +0100

    f****** Drugs man!!!

---
+++ Fysik-el-VR/Assets/Scripts/HealthItemAdd.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthItemAdd : MonoBehaviour
+{
+    int AddingHealth;
+    private int DeSpawnTime = 2;
+    private OVRGrabbable ovrGrabbable;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ovrGrabbable = GetComponent<OVRGrabbable>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ovrGrabbable.isGrabbed)
+        {
+
+        }
+    }
+}
