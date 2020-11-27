ebe40577e56fbbf6efb62842fef6d65e910c1ace
Mongoose92 <micch15@student.sdu.dk>
2020-11-27 10:24:32 +0100

    New ItemSpawner

---
+++ Fysik-el-VR/Assets/Scripts/ItemSpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawner : MonoBehaviour
+{
+    public GameObject item;
+    public GameObject inventorySlot;
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+
+        SpawnItem();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (item.transform.position != inventorySlot.transform.position)
+        {
+            SpawnItem();
+        }
+    }
+
+    public void SpawnItem()
+    {
+        Instantiate(item, inventorySlot.tansform.position, Quaternion.identity);
+        return;
+    }
+    
+    
+
+    
+
+  
+
+}
