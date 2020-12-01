1338dcadfd8a0c5e59f115fcdcb10fd9b5ccacec
Nickolai <nipet18@student.sdu.dk>
2020-12-01 12:59:21 +0100

    Item spawn & UI Dialogue

---
+++ Fysik-el-VR/Assets/Scripts/IntroRoom/CreateObject.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreateObject : MonoBehaviour
+{
+    public Transform BattteriSpawnpoint;
+    public Transform ResistorSpawnPoint;
+
+    public Rigidbody BatteriPrefab;
+    public Rigidbody ResistorPrefab;
+
+    public bool BatteriIsspawned = false;
+    public bool ResistorIsSpawn = false;
+
+    void OnTriggerEnter ()
+    {
+
+        if (!BatteriIsspawned)
+        {
+            Rigidbody batPrefab;
+            batPrefab = Instantiate(BatteriPrefab, BattteriSpawnpoint.position, BattteriSpawnpoint.rotation) as Rigidbody;
+            BatteriIsspawned = true;
+        } else if (!ResistorIsSpawn)
+        {
+            Rigidbody resPrefab;
+            resPrefab = Instantiate(ResistorPrefab, ResistorSpawnPoint.position, ResistorSpawnPoint.rotation) as Rigidbody;
+            ResistorIsSpawn = true;
+        }        
+    }
+}
---
+++ Fysik-el-VR/Assets/Scripts/IntroRoom/DialogueTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour{
+
+    public Dialogue dialogue;
+
+    public void TriggerDialogue()
+    {
+        //best to used a singleton but lets us keep it simple//
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+}
