be36a614a1be5d5fd18a2cfd5aa7c8902865af36
Jejak-14 <jejak14@student.sdu.dk>
2020-12-04 15:46:55 +0100

    level 1 build Start

---
+++ Fysik-el-VR/Assets/Scripts/DoorStart.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorStart : MonoBehaviour
+{
+
+    public GameObject RDoor;
+    public GameObject LDoor;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        StartCoroutine(DoorOpens());
+        StartCoroutine(DoorOpens2());
+    }
+
+    IEnumerator DoorOpens()
+    {
+        do
+        {
+            RDoor.GetComponent<DoorOpeningScript>().doorOpen();
+            yield return new WaitForEndOfFrame();
+            //while the X-pos is not right (the axis that we open the door on)
+        } while (RDoor.transform.position.x != RDoor.GetComponent<DoorOpeningScript>().xPos);
+
+    }
+
+    //sends signal to open the left door as long as it's not in "open" position
+    IEnumerator DoorOpens2()
+    {
+        do
+        {
+            LDoor.GetComponent<DoorOpeningScript>().doorOpen();
+            yield return new WaitForEndOfFrame();
+            //while the X-pos is not right (the axis that we open the door on)
+        } while (LDoor.transform.position.x != LDoor.GetComponent<DoorOpeningScript>().xPos);
+    }
+}
