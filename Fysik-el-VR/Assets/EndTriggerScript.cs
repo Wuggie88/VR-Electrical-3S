c4bed6e9ef742576d01cd257dfc7907ea46de8d7
Wuggie <wuggie88@yahoo.co.uk>
2020-12-11 08:58:12 +0100

    blocked off hallway with screen thanking people for playing prototype

---
+++ Fysik-el-VR/Assets/EndTriggerScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndTriggerScript : MonoBehaviour
+{
+  
+    bool endTextShown = false;
+    public Dialogue dialogue;
+    public GameObject dialogueManager;
+
+
+
+    public void OnTriggerEnter(Collider collider)
+    {
+        //Starting the dialog//
+        if(collider.tag == "Player")
+        {
+            //Debug.Log("EndTrigger entered");
+            if (endTextShown == false)
+            {
+                //Debug.Log("endDialogueStarted");
+                dialogueManager.GetComponent<DialogueManager>().StartDialogue(dialogue);
+                endTextShown = true;
+            }
+        }
+    }
+}
