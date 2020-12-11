5c10302fa1c3e22ffa5030a3a4e8551c8797aabe
Anders Gaarsdal <angaa19@student.sdu.dk>
2020-12-11 14:02:52 +0100

    Lavet snapping script mere compact

--- Fysik-el-VR/Assets/Scripts/Snapping.cs
+++ Fysik-el-VR/Assets/Scripts/Snapping.cs
@@ -48,29 +48,14 @@ public class Snapping : MonoBehaviour
             // if there isn't an object it will check if the snappoints type is the same as the objects tag.
             if (componentType == ComponentType.Battery && other.tag == "Battery")
             {
-                // here it then sets the object as the snapped object, where we then get so of its components
+                // here it then sets the object as the snapped object, and calls the method Snap()
                 snapObject = other.gameObject;
-                snapBody = snapObject.GetComponent<Rigidbody>();
-                snapBody.useGravity = false;
-                ovrGrabbable = other.GetComponent<OVRGrabbable>();
-
-                snapped = true;
-                
-                // here we get the value of the component, which we use for the calculation
-                values = snapObject.GetComponent<componentScript>().value;
-                circuitBoard.GetComponent<CircuitScript>().setupComponent();
+                Snap(snapObject);
             }
             else if (componentType == ComponentType.OtherComponent && other.tag == "Component")
             {
                 snapObject = other.gameObject;
-                snapBody = snapObject.GetComponent<Rigidbody>();
-                snapBody.useGravity = false;
-                ovrGrabbable = other.GetComponent<OVRGrabbable>();
-
-                snapped = true;
-                
-                values = snapObject.GetComponent<componentScript>().value;
-                circuitBoard.GetComponent<CircuitScript>().setupComponent();
+                Snap(snapObject);
             }
             hasSnapObject = true;
         }
@@ -82,5 +67,20 @@ public class Snapping : MonoBehaviour
         // when the object leaves the snappoint, its physics is returned to normal
         hasSnapObject = false;
         snapBody.useGravity = true;
+    }
+
+    void Snap(GameObject snapObject)
+    {
+        // here we get the objects components and sets the obejects gravity to false
+        snapBody = snapObject.GetComponent<Rigidbody>();
+        snapBody.useGravity = false;
+        ovrGrabbable = snapObject.GetComponent<OVRGrabbable>();
+
+        snapped = true;
+
+        // here we get the value of the component, which we use for the calculation
+        values = snapObject.GetComponent<componentScript>().value;
+        circuitBoard.GetComponent<CircuitScript>().setupComponent();
+
     }
 }
